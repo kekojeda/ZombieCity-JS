5c1d7ddbd325b36fb2a3a39bec4d0078cd02573b
Sergio Ojeda <sergio.ojeda@vstudios.local>
2019-09-02 14:48:19 -0300

    first commit

---
+++ js/zombie_conductor.js
@@ -0,0 +1,15 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov/*, parametro/s extra de ZombieConductor*/) {
+  /* Completar constructor a partir de Enemigo */
+  //Enemigo.call(/* ... */);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+}
+
+/* Completar creacion del ZombieConductor */
+
+/* Completar metodos para el movimiento y el ataque */
